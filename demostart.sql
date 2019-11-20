c32b3f188a7c11a6324a03964886ac94339dafbe
TMNAlice <kostyalistov@yandex.ru>
2019-11-20 09:50:27 +0300

    html done

--- demostart.sql
+++ demostart.sql
@@ -20,3 +20,6 @@ CREATE TABLE  (
 
 Insert into devtab (number, name) values (1, "qwerty");
 Insert into devtab (number, name) values (2, "b2");
+
+--login guest password hello
+insert into "user"("login", "pass_hash") values ('guest','$2a$10$6mf3CesQx9eRGB4B3sjr8e1eSr5cYO/zt87bwYVdA4O8rmjDMDdHO');
--- src/main/java/com/example/demo/config/ControllersConfig.java
+++ src/main/java/com/example/demo/config/ControllersConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
